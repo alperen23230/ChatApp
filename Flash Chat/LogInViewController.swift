0f444cbbdd8bf0fffa23685bd08110f7c8096886
Alperen Ünal <alperenunal@Alperens-MacBook-Air.local>
2018-10-09 10:37:42 +0300

    Initial Commit

---
+++ Flash Chat/LogInViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LogInViewController.swift
+//  Flash Chat
+//
+//  This is the view controller where users login
+
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+
+class LogInViewController: UIViewController {
+
+    //Textfields pre-linked with IBOutlets
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+   
+    @IBAction func logInPressed(_ sender: AnyObject) {
+
+        SVProgressHUD.show()
+        
+        //TODO: Log in the user
+        Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user,error ) in
+            if error != nil{
+                print(error!)
+            }
+            else{
+                print("Login Successful")
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "goToChat", sender: self)
+            }
+            
+        }
+    }
+    
+
+
+    
+}  
